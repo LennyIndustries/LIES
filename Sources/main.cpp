bbde0f804271b936e315deae17315666006b6226
Lenny <leander.dumas@live.be>
2022-03-18 15:16:45 +0100

    IDE Setup

---
+++ Sources/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * Created by Leander @ Lenny Industries on 18/03/2022.
+ * Project: Project_TBD.
+ * Copyright (c) 2022 Lenny Industries. All rights reserved.
+ */
+
+// Libraries
+#include <cstdio>
+
+// Definitions
+
+// Error codes
+#define ERR_0 0 // No error
+
+// Functions
+
+int main(int argc, char **argv)
+{
+	printf("Hello World");
+	
+	return ERR_0;
+}
